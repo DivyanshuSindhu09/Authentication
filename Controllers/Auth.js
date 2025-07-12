0a86c98a5799a598fef15f7d11479b1145fe5a5d
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2025-07-12 20:17:46 +0530

    doneee

--- Controllers/Auth.js
+++ Controllers/Auth.js
@@ -2,6 +2,9 @@
 
 const bcrypt = require('bcrypt')
 const User = require('../models/User')
+const  jwt = require('jsonwebtoken')
+
+require('dotenv').config()
 
 exports.signup = async (req, res) => {
     try {
@@ -28,7 +31,7 @@ exports.signup = async (req, res) => {
         }
 
     // create entry for new user
-        const user = await User.create({
+        let user = await User.create({
             name, email, password : hashedPassword, role
         })
     return res.status(200).json({
@@ -42,6 +45,81 @@ exports.signup = async (req, res) => {
         return res.status(500).json({
             success : false,
             message : 'An error occured'
+        })
+    }
+}
+
+// login 
+
+exports.login = async (req, res) => {
+    try {
+    // get data
+        const {email, password} = req.body
+    // no data
+    if(!email || !password){
+        return res.status(400).json({
+            success : false,
+            message : 'Please Fill All The Details Carefully'
+        })
+    }
+
+    // checking for registered user
+
+    const user = await User.findOne({email})
+
+    // if not a registered user
+    if(!user){
+        return res.status(401).json({
+            success : false,
+            message : 'user does not exists'
+        })
+    }
+
+    // verify password 
+
+    const payload = {
+        email : user.email,
+        id : user._id,
+        role : user.role
+    }
+
+    if(await bcrypt.compare(password, user.password)){
+    // correct password
+        let token = jwt.sign(payload,
+                            process.env.JWT_SECRET,
+                            {
+                                expiresIn : '2h'
+                            })
+
+        // user = user.toObject()
+        user.token = token
+        user.password = undefined
+
+        const options = {
+            expires : new Date( Date.now() + 3 * 24 * 60 * 60 * 1000 ),
+            httpOnly : true 
+        }
+    // creating cookie - 3 params name, data & few options
+        res.cookie('token', token, options).status(200).json({
+            success : true,
+            message  : 'User Logged In Successfully',
+            token,
+            user
+        })
+    }else{
+    // wrong password
+    return res.status(400).json({
+        success : false,
+        message : 'Invalid Password'
+    })
+    }
+
+    } catch (error) {
+        console.log("Error")
+        console.log(error)
+        return res.status(500).json({
+            success :false,
+            message : 'An Error Occured'
         })
     }
 }
